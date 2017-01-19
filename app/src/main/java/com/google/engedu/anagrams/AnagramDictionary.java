76acfacbe369db3baeac204a00a0bc55c5ef133c
swati4star <swati4star@gmail.com>
2017-01-19 22:35:20 +0530

    adding functions to AnagramDictionary

--- app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
+++ app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -19,39 +19,104 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
-public class AnagramDictionary {
+class AnagramDictionary {
 
     private static final int MIN_NUM_ANAGRAMS = 5;
     private static final int DEFAULT_WORD_LENGTH = 3;
     private static final int MAX_WORD_LENGTH = 7;
     private Random random = new Random();
+    private HashSet wordSet;
+    private ArrayList wordList;
+    private HashMap lettersToWord;
 
-    public AnagramDictionary(Reader reader) throws IOException {
+    AnagramDictionary(Reader reader) throws IOException {
         BufferedReader in = new BufferedReader(reader);
-        String line;
+        String line, key;
+        wordSet = new HashSet();
+        wordList = new ArrayList();
+        lettersToWord = new HashMap();
         while((line = in.readLine()) != null) {
             String word = line.trim();
+            wordSet.add(word);
+            wordList.add(word);
+            ArrayList arrayList = new ArrayList();
+            key = alphabeticalOrder(word);
+            if(!lettersToWord.containsKey(key)){
+                arrayList.add(word);
+                lettersToWord.put(key,arrayList);
+            }
+            else{
+                arrayList = (ArrayList) lettersToWord.get(key);
+                arrayList.add(word);
+                lettersToWord.put(key, arrayList);
+            }
         }
     }
 
+    /**
+     * Verifies if word is good for a given base word
+     * @param word  Word to be checked
+     * @param base  Base word
+     * @return      boolean : true if word is good
+     */
     public boolean isGoodWord(String word, String base) {
-        return true;
+        // Word should not be subset
+        if (wordSet.contains(word) && !base.contains(word))
+            return true;
+        else
+            return false;
     }
 
-    public List<String> getAnagrams(String targetWord) {
-        ArrayList<String> result = new ArrayList<String>();
-        return result;
-    }
-
+    /**
+     * Returns all possible words for a given word by adding one more letter
+     * @param word  Base Word
+     * @return      Array lit containing possible words
+     */
     public List<String> getAnagramsWithOneMoreLetter(String word) {
+        ArrayList<String> resultant;
         ArrayList<String> result = new ArrayList<String>();
+        for(char alphabet = 'a'; alphabet <= 'z';alphabet++) {
+            String newWord = word + alphabet;
+            String extendedKey = alphabeticalOrder(newWord);
+            if(lettersToWord.containsKey(extendedKey) ){
+                resultant = (ArrayList) lettersToWord.get(extendedKey);
+                for(int i = 0;i< resultant.size();i++)
+                    result.add(String.valueOf(resultant.get(i)));
+            }
+        }
         return result;
     }
 
+    /**
+     * Returns a valid and good starter word
+     * @return  A starter word
+     */
     public String pickGoodStarterWord() {
-        return "stop";
+
+        while(true) {
+            int num = random.nextInt(wordList.size());
+            String word = (String) wordList.get(num);
+            ArrayList res = (ArrayList) lettersToWord.get(alphabeticalOrder(word));
+            // A good word's length should be greater than 7 && must have atleast 5 anagrams
+            if(word.length() <= MAX_WORD_LENGTH && res.size() >= MIN_NUM_ANAGRAMS)
+                return (String) wordList.get(num);
+        }
+    }
+
+    /**
+     * Sorts given word in ascending order
+     * @param word  word to be sorted
+     * @return      sorted word
+     */
+    public String alphabeticalOrder(String word){
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
 }
--- app/src/main/java/com/google/engedu/anagrams/AnagramsActivity.java
+++ app/src/main/java/com/google/engedu/anagrams/AnagramsActivity.java
@@ -126,7 +126,7 @@ public class AnagramsActivity extends AppCompatActivity {
         TextView resultView = (TextView) findViewById(R.id.resultView);
         if (currentWord == null) {
             currentWord = dictionary.pickGoodStarterWord();
-            anagrams = dictionary.getAnagrams(currentWord);
+            anagrams = dictionary.getAnagramsWithOneMoreLetter(currentWord);
             gameStatus.setText(Html.fromHtml(String.format(START_MESSAGE, currentWord.toUpperCase(), currentWord)));
             fab.setImageResource(android.R.drawable.ic_menu_help);
             fab.hide();
